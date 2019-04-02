d14a6d62ad2b50811fb23a9b29e568d4568747bc
Kunal Mhatre <kunalmhatre@protonmail.com>
2019-04-02 11:09:27 +0530

    Added themes to GlobalStyle

--- app/global-styles.js
+++ app/global-styles.js
@@ -1,18 +1,39 @@
 import { createGlobalStyle } from 'styled-components';
 
-const GlobalStyle = createGlobalStyle`
-  html,
-  body {
-    height: 100%;
-    width: 100%;
-    font-family: 'Roboto', Helvetica, Arial, sans-serif;
-    font-size: 18px;
-  }
+const getGlobalStyle = (theme) => {
+  switch (theme) {
+    case 'dark': return createGlobalStyle`
+      html,
+      body {
+        height: 100%;
+        width: 100%;
+        font-family: 'Roboto', Helvetica, Arial, sans-serif;
+        font-size: 18px;
+        background: #000000;
+      }
+    
+      #app {
+        height: 100%;
+        width: 100%;
+      }
+    `;
 
-  #app {
-    min-height: 100%;
-    min-width: 100%;
+    default: return createGlobalStyle`
+      html,
+      body {
+        height: 100%;
+        width: 100%;
+        font-family: 'Roboto', Helvetica, Arial, sans-serif;
+        font-size: 18px;
+        background: #004392;
+      }
+    
+      #app {
+        height: 100%;
+        width: 100%;
+      }
+    `;
   }
-`;
+};
 
-export default GlobalStyle;
+export default getGlobalStyle;
