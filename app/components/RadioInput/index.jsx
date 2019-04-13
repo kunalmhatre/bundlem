3999c5136d14908c34716b94b202be389fba542e
Kunal Mhatre <kunalmhatre@protonmail.com>
2019-04-13 18:18:46 +0530

    Added RadioInput component

---
+++ app/components/RadioInput/index.jsx
@@ -0,0 +1,110 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage, injectIntl, intlShape } from 'react-intl';
+import shortid from 'shortid';
+import classNames from 'classnames';
+
+import messages from './messages';
+import './radio-input.css';
+
+const formattedMessagePropTypes = PropTypes.shape({
+  id: PropTypes.string.isRequired,
+  defaultMessage: PropTypes.string.isRequired,
+});
+
+const propTypes = {
+  items: PropTypes.arrayOf(formattedMessagePropTypes),
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+  showError: PropTypes.bool,
+  errorMessage: formattedMessagePropTypes,
+  label: formattedMessagePropTypes,
+  name: PropTypes.string,
+  onBlur: PropTypes.func,
+  intl: intlShape.isRequired,
+};
+
+const defaultProps = {
+  items: [messages.defaultRadioItem],
+  value: '',
+  onChange: () => null,
+  showError: false,
+  errorMessage: messages.defaultErrorMessage,
+  label: messages.defaultRadioLabel,
+  name: shortid.generate(),
+  onBlur: () => null,
+};
+
+/**
+ * RadioInput component
+ * @param {array} items - Radio inputs (items) to be rendered
+ * @param {number} value - Value of the selected item
+ * @param {function} onChange - On change handler function
+ * @param {boolean} [showError=false] - Shows error below the input field when true
+ * @param {object} errorMessage - Error message to be shown
+ * @param {object} label - Label for the radio input
+ * @param {string} name - Sets name attribute for the radio input field
+ * @param {function} onBlur - On blur handler function
+ */
+function RadioInput({
+  items,
+  value,
+  onChange,
+  showError,
+  errorMessage,
+  label,
+  name,
+  onBlur,
+  intl,
+}) {
+  return (
+    <div className="radio-input-container theme-blue">
+      <div className="radio-input-main-label">
+        <FormattedMessage {...label} />
+      </div>
+      <ul className="radio-input-group">
+        {
+          items.map((item, index) => {
+            const valueFM = intl.formatMessage(item);
+            const classes = classNames(
+              { 'radio-input': true },
+              { 'theme-blue-inverse': value === valueFM },
+            );
+
+            return (
+              <li
+                key={shortid.generate()}
+                className={classes}
+              >
+                <label
+                  htmlFor={`radio-input-${index}`}
+                  className="radio-input-label"
+                >
+                  <FormattedMessage {...item} />
+                  <input
+                    id={`radio-input-${index}`}
+                    type="radio"
+                    name={name}
+                    className="radio-input-field"
+                    value={valueFM}
+                    checked={value === valueFM}
+                    onChange={onChange}
+                    onBlur={onBlur}
+                  />
+                </label>
+              </li>
+            );
+          })
+        }
+      </ul>
+      <div className="radio-input-error-message theme-blue-error-message">
+        { showError ? <FormattedMessage {...errorMessage} /> : null }
+      </div>
+    </div>
+  );
+}
+
+RadioInput.propTypes = propTypes;
+RadioInput.defaultProps = defaultProps;
+
+export default React.memo(injectIntl(RadioInput));
---
+++ app/components/RadioInput/tests/index.test.jsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import { mountWithIntl } from '../../../../internals/testing/reactIntlHelperFunction';
+
+import RadioInput from '../index';
+
+describe('<RadioInput />', () => {
+  const items = [
+    {
+      id: 'testID1',
+      defaultMessage: 'testMessage1',
+    },
+    {
+      id: 'testID2',
+      defaultMessage: 'testMessage2',
+    },
+  ];
+
+  it('renders 2 radio inputs', () => {
+    expect(
+      mountWithIntl(
+        <RadioInput items={items} />,
+      ).find('input')
+        .length,
+    ).toBe(2);
+  });
+
+  it('calls on change handler', () => {
+    const onChange = jest.fn(event => event.target.value);
+
+    mountWithIntl(
+      <RadioInput
+        items={items}
+        onChange={onChange}
+      />,
+    ).find('input')
+      .at(0)
+      .simulate(
+        'change',
+        {
+          target: {
+            checked: true,
+            value: items[0].defaultMessage,
+          },
+        },
+      );
+
+    expect(onChange).toHaveReturnedWith('testMessage1');
+  });
+
+  it('calls default on change handler when none is provided', () => {
+    mountWithIntl(
+      <RadioInput items={items} />,
+    ).find('input')
+      .at(0)
+      .simulate(
+        'change',
+        {
+          target: {
+            checked: true,
+          },
+        },
+      );
+  });
+
+  it('calls default on blur handler function', () => {
+    mountWithIntl(
+      <RadioInput items={items} />,
+    ).find('input')
+      .at(0)
+      .simulate('blur');
+  });
+
+  it('shows correct error message', () => {
+    expect(
+      mountWithIntl(
+        <RadioInput
+          showError
+          items={items}
+          errorMessage={items[1]}
+        />,
+      ).find('div.radio-input-error-message')
+        .text(),
+    ).toBe(items[1].defaultMessage);
+  });
+});
