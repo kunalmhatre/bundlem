260f2513913698062bc5e98cc98c6d8a18d1a3f6
Kunal Mhatre <kunalmhatre@protonmail.com>
2019-04-17 02:22:44 +0530

    Added APIs

---
+++ app/utils/apis.js
@@ -0,0 +1,8 @@
+/**
+ * REST APIs
+ */
+
+export const domain = 'https://shielded-mesa-38298.herokuapp.com';
+
+export const verifyTokenAPI = `${domain}/verify`;
+export const publishBundleAPI = `${domain}/create`;
