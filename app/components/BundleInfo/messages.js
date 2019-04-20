c756a0eb49f8e7205d62b924c8adcf941188a95a
Kunal Mhatre <kunalmhatre@protonmail.com>
2019-04-21 01:01:02 +0530

    BundleInfo component

---
+++ app/components/BundleInfo/messages.js
@@ -0,0 +1,16 @@
+/*
+ * BundleInfo Messages
+ *
+ * This contains all the text for the BundleInfo component.
+ */
+
+import { defineMessages } from 'react-intl';
+
+export const scope = 'app.components.BundleInfo';
+
+export default defineMessages({
+  resources: {
+    id: `${scope}.resources`,
+    defaultMessage: 'Resources: ',
+  },
+});
