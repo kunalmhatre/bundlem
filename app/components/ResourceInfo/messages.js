d327a5022fbec52e862330f4d354474a983e165a
Kunal Mhatre <kunalmhatre@protonmail.com>
2019-04-21 01:29:27 +0530

    Added ResourceInfo component

---
+++ app/components/ResourceInfo/messages.js
@@ -0,0 +1,52 @@
+/*
+ * ResourceInfo Messages
+ *
+ * This contains all the text for the ResourceInfo component.
+ */
+
+import { defineMessages } from 'react-intl';
+
+export const scope = 'app.components.ResourceInfo';
+
+export default defineMessages({
+  'actionWEB PAGE': {
+    id: `${scope}.actionWEB PAGE`,
+    defaultMessage: 'VISIT',
+  },
+  actionVIDEO: {
+    id: `${scope}.actionVIDEO`,
+    defaultMessage: 'WATCH',
+  },
+  actionDOCUMENT: {
+    id: `${scope}.actionDOCUMENT`,
+    defaultMessage: 'VIEW',
+  },
+  actionIMAGE: {
+    id: `${scope}.actionIMAGE`,
+    defaultMessage: 'VIEW',
+  },
+  actionOTHER: {
+    id: `${scope}.actionOTHER`,
+    defaultMessage: 'VIEW',
+  },
+  'WEB PAGE': {
+    id: `${scope}.WEB PAGE`,
+    defaultMessage: 'WEB PAGE',
+  },
+  VIDEO: {
+    id: `${scope}.VIDEO`,
+    defaultMessage: 'VIDEO',
+  },
+  DOCUMENT: {
+    id: `${scope}.DOCUMENT`,
+    defaultMessage: 'DOCUMENT',
+  },
+  IMAGE: {
+    id: `${scope}.IMAGE`,
+    defaultMessage: 'IMAGE',
+  },
+  OTHER: {
+    id: `${scope}.OTHER`,
+    defaultMessage: 'OTHER',
+  },
+});
