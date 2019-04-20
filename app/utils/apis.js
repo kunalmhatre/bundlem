3c82319cedb3ac88dcb5a62cf259bed0d655eb84
Kunal Mhatre <kunalmhatre@protonmail.com>
2019-04-21 01:27:27 +0530

    Added API to get bundles

--- app/utils/apis.js
+++ app/utils/apis.js
@@ -6,3 +6,4 @@ export const domain = 'https://shielded-mesa-38298.herokuapp.com';
 
 export const verifyTokenAPI = `${domain}/verify`;
 export const publishBundleAPI = `${domain}/create`;
+export const getBundleAPI = `${domain}/bundle`;
