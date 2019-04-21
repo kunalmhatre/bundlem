9739ec5abdb7891ab1bb09619f874f9a1ae2b660
Kunal Mhatre <kunalmhatre@protonmail.com>
2019-04-21 13:07:41 +0530

    Added new routes

--- app/containers/App/index.jsx
+++ app/containers/App/index.jsx
@@ -10,19 +10,29 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import HomePage from 'containers/HomePage/Loadable';
-import NotFoundPage from 'containers/NotFoundPage/Loadable';
-
-import GlobalStyle from '../../global-styles';
+import NotFoundPage from '../NotFoundPage/Loadable';
+import Bundle from '../Bundle/Loadable';
+import Make from '../Make/Loadable';
+import Create from '../Create/Loadable';
+import Robot from '../Robot/Loadable';
+import Welcome from '../../components/Welcome/Loadable';
+import ProtectedRoute from '../../components/ProtectedRoute';
+import getGlobalStyle from '../../global-styles';
+import '../../themes.css';
 
 export default function App() {
+  const GlobalStyle = getGlobalStyle();
   return (
-    <div>
+    <React.Fragment>
       <Switch>
-        <Route exact path="/" component={HomePage} />
+        <Route exact path="/" component={Welcome} />
+        <Route path="/create" component={Create} />
+        <ProtectedRoute path="/make" component={Make} />
+        <ProtectedRoute path="/robot" component={Robot} />
+        <Route path="/bundle" component={Bundle} />
         <Route component={NotFoundPage} />
       </Switch>
       <GlobalStyle />
-    </div>
+    </React.Fragment>
   );
 }
