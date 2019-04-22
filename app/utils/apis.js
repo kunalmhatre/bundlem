1d8c04e695cd96d020599c621ba77d54c49d5b32
Kunal Mhatre <kunalmhatre@protonmail.com>
2019-04-22 23:43:18 +0530

    Bug fix - Critical

--- app/utils/apis.js
+++ app/utils/apis.js
@@ -2,7 +2,7 @@
  * REST APIs
  */
 
-export const domain = 'https://bundlem.in';
+export const domain = 'https://bundlem.herokuapp.com';
 
 export const verifyTokenAPI = `${domain}/verify`;
 export const publishBundleAPI = `${domain}/create`;
