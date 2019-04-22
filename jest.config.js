9bc578822473a3004883e83f0df417dd3bea7360
Kunal Mhatre <kunalmhatre@protonmail.com>
2019-04-22 15:28:05 +0530

    Updated coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -9,10 +9,10 @@ module.exports = {
   ],
   coverageThreshold: {
     global: {
-      statements: 80,
-      branches: 80,
-      functions: 80,
-      lines: 80,
+      statements: 100,
+      branches: 100,
+      functions: 100,
+      lines: 100,
     },
   },
   moduleDirectories: ['node_modules', 'app'],
